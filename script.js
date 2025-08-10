61d15170cf39550f5baea4ab15d48af638244ba3
que <kosstetz@gmail.com>
2025-08-10 18:52:59 +0300

    Revert "failed attempt"
    This reverts commit 46f68c9dc55f304e88f7466bafd0cf8bb78ac2ae.

---
+++ script.js
@@ -0,0 +1,52 @@
+import quotes from "./src/data/quotes.js";
+import {
+  toggleFavoriteIcon,
+  renderFavorites,
+} from "./src/handlers/favorites.js";
+import { toggleTheme, applyTheme } from "./src/handlers/theme.js";
+import { toggleFavoriteStatus } from "./src/handlers/quote.js";
+import { getRandomint } from "./src/utils/math.js";
+
+// DOM elements
+const generateBtn = document.getElementById("generate-btn");
+const favoriteButton = document.getElementById("favorite-btn");
+const favoritesField = document.getElementById("favorites-field");
+let lastIndex = -1;
+
+const quoteElement = document.getElementById("quote");
+const quoteAuthorElement = document.getElementById("quote-author");
+
+function generateRandomQuote() {
+  const { quote, author, favorite, index } = getRandomint(quotes, lastIndex);
+  lastIndex = index;
+  quoteElement.textContent = `"${quote}"`;
+  quoteAuthorElement.textContent = author;
+  toggleFavoriteIcon(favorite, favoriteButton);
+}
+
+function toggleFavorite(index) {
+  const isFavorite = toggleFavoriteStatus(quotes, index);
+  toggleFavoriteIcon(isFavorite, favoriteButton);
+  renderFavorites(quotes, favoritesField, handleDeleteFavorite);
+}
+
+function handleDeleteFavorite(index) {
+  quotes[index].favorite = false;
+  renderFavorites(quotes, favoritesField, handleDeleteFavorite);
+  if (lastIndex === index) {
+    toggleFavoriteIcon(false, favoriteButton);
+  }
+}
+
+// Theme logic
+const themeBtn = document.getElementById("theme-btn");
+const themeImg = document.getElementById("themeimg");
+applyTheme(themeImg);
+themeBtn.addEventListener("click", () => toggleTheme(themeImg));
+
+// Event listeners
+generateBtn.addEventListener("click", generateRandomQuote);
+favoriteButton.addEventListener("click", () => toggleFavorite(lastIndex));
+
+// Initial render of favorites
+renderFavorites(quotes, favoritesField, handleDeleteFavorite);
